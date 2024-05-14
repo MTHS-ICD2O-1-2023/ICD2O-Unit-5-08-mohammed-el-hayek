0a3ddade3c7d91a227cfc2fb56b1bf26f9dbf707
mohammed-el-hayek <159185058+mohammed-el-hayek@users.noreply.github.com>
2024-05-14 02:45:45 +0000

    v5-08.1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mohammed EL-hayek All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mohammed
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+  //this function divides using a loop
+  //input
+  let number1 = parseInt(document.getElementById("number-1").value);
+  let number2 = parseInt(document.getElementById("number-2").value);
+
+  //process
+  let counter = 0;
+  while (number1 >= number2) {
+    number1 -= number2;
+    counter++
+  }
+
+  //output
+  if (number1 != 0) {
+    document.getElementById('answer').innerHTML = "the answer is" + counter + "with a remainder of" + number1;
+  } else {
+    document.getElementById('answer').innerHTML = "the answer is" + counter;
+  }
+}
